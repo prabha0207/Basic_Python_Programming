d241860e20cefaecc8cd2c804ad2670d8eb9d33d
Swarnaprabha M <swarnaprabhamaha@gmail.com>
2019-06-12 16:17:09 +0530

    Create pr53q.py

---
+++ pr53q.py
@@ -0,0 +1,11 @@
+string=str(input())
+string=string.lower()
+l=len(string)
+L=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(0,l):
+	if(string[i] in L):
+		L.remove(string[i])
+if(len(L)==0):
+	print("yes")
+else:
+	print("no")
